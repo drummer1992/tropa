c02eaa39934005cdfc230bd8738560346f47a5c0
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2021-04-17 16:51:49 +0300

    - commit missed lines

--- app-example/services/app-service.js
+++ app-example/services/app-service.js
@@ -1,6 +1,8 @@
 import BaseService from "../../lib/base-service"
 import { ErrorHandler } from "./decorators/error-handler"
+import { ApiPrefix } from "../../lib/decorators"
 
+@ApiPrefix('/api/v2')
 export default class AppService extends BaseService {
   @ErrorHandler
   async execute() {
