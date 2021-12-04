9fe7703f736aef2522e22197f2b6317c0551e3de
Andrii Varlamov <andrii.varlamov@backendless.consulting>
2021-12-05 01:27:12 +0200

    [app-example] - code refactoring

---
+++ app-example/hooks.js
@@ -0,0 +1,51 @@
+import Hooks from 'tropa/lib/hooks'
+import { ApiError } from './errors'
+
+const compact = (...args) => args.filter(Boolean)
+const serialize = value => JSON.stringify(value)
+
+export default class AppHooks extends Hooks {
+  onRequest(ctx) {
+    ctx.start = new Date()
+  }
+
+  onHandler(ctx) {
+    console.log(...compact(
+      ctx.start.toISOString(),
+      `[${ctx.request.id}]`,
+      '[REQ]',
+      ctx.request.method,
+      ctx.request.url,
+      ctx.request.param && 'PARAM: ' + serialize(ctx.request.param),
+      ctx.request.query && 'QUERY: ' + serialize(ctx.request.query),
+      ctx.request.body && 'BODY: ' + serialize(ctx.request.body),
+    ))
+  }
+
+  onError(err, ctx) {
+    if (!(err instanceof ApiError)) {
+      console.error(err)
+
+      err = new ApiError('Internal Server Error', 500)
+    }
+
+    ctx.response.statusCode = err.statusCode
+
+    return err
+  }
+
+  onResponse(ctx) {
+    const end = new Date()
+
+    console.log(...compact(
+      end.toISOString(),
+      `[${ctx.request.id}]`,
+      '[RES]',
+      ctx.response.body && 'BODY: ' + serialize(ctx.response.body),
+    ))
+
+    const ms = end.getTime() - ctx.start.getTime()
+
+    console.log(`[${ctx.request.id}]`, `Processing finished. ${ms} ms`)
+  }
+}
